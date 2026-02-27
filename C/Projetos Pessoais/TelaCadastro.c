5da5042830da1cb8a796b025ddb5669490344e47
ArthurAlpe <alpe.pessoal@gmail.com>
2026-02-26 23:52:11 -0300

    Att Projeto Pessoal

--- C/Projetos Pessoais/TelaCadastro.c
+++ C/Projetos Pessoais/TelaCadastro.c
@@ -6,32 +6,12 @@ Objetivo: Tela de cadastro
 
 #include <stdio.h>
 #include <windows.h>
+
 //Funcao para tela
+void tela(){
 
-void gotoxy(int x, int y) 
-{
-
-    COORD coord;
-    coord.X = (short)x;
-    coord.Y = (short)y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
-
-//Programa principal
-int main (){
-
-    //Variaveis
-    int opcao;
-    char nome[50];
-    int idade;
-    char email[50];
-    int l;
     int c;
-
-    //Começo do programa
-
-    system("color 02");
-    system("cls");
+    int l;
 
     for ( l = 1; l < 25; l++) {
         gotoxy(01,l);
@@ -75,7 +55,32 @@ int main (){
     printf("Estrutura de Dados");
     gotoxy(67,3);
     printf("Prof. Rodney");
-    
+}
+//Funcao para gotoxy
+void gotoxy(int x, int y) 
+{
+
+    COORD coord;
+    coord.X = (short)x;
+    coord.Y = (short)y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+//Programa principal
+int main (){
+
+    //Variaveis
+    int opcao;
+    char nome[50];
+    int idade;
+    char email[50];
+
+    //Começo do programa
+
+    system("color 02");
+    system("cls");
+
+    tela();
     gotoxy(24,5);
     printf("TELA DE CADASTRO");
     gotoxy(24,9);
@@ -95,71 +100,28 @@ int main (){
 
     while (opcao != 0) { 
         
+        system("cls");
+
         switch (opcao) {
 
             case 1:
             system("cls");
-            for ( l = 1; l < 25; l++) {
-                gotoxy(01,l);
-                printf("|");
-                gotoxy(80,l);
-                printf("|");
-            }
-            
-            for ( c = 1; c < 81; c++) {
-                gotoxy(c,01);
-                printf("-");
-                gotoxy(c,04);
-                printf("-");
-                gotoxy(c,22);
-                printf("-");
-                gotoxy(c,24);
-                printf("-");
-            }
-
-            gotoxy(01,1);
-            printf("+");
-            gotoxy(80,1);
-            printf("+");
-            gotoxy(1,22);
-            printf("+");
-            gotoxy(1,24);
-            printf("+");
-            gotoxy(80,24);
-            printf("+");
-            gotoxy(80,22);
-            printf("+");
-            gotoxy(01,4);
-            printf("+");
-            gotoxy(80,4);
-            printf("+");
-            gotoxy(2,2);
-            printf("UNICV");
-            gotoxy(2,3);
-            printf("Arthur Almeida Pereira 2 Semestre ADS/ESW");
-            gotoxy(62,2);
-            printf("Estrutura de Dados");
-            gotoxy(67,3);
-            printf("Prof. Rodney");
+            tela();
             
             gotoxy(24,5);
             printf("TELA DE CADASTRO");
 
             gotoxy(24,9);
             printf("Nome: ");
-
+            gotoxy(24,11);
+            printf("Idade: ");
+            gotoxy(24,13);
+            printf("Email: ");
             gotoxy(30,9);
             fflush(stdin);
             fgets(nome, 50, stdin);
-
-            gotoxy(24,11);
-            printf("Idade: ");
-
             gotoxy(31,11);
-            scanf("%d", &idade);
-
-            gotoxy(24,13);
-            printf("Email: ");
+            scanf("%d", &idade);  
 
             gotoxy(31,13);
             fflush(stdin);
@@ -176,64 +138,6 @@ int main (){
             gotoxy(07,23);
             scanf("%d", &opcao);
 
-            for ( l = 1; l < 25; l++) {
-                gotoxy(01,l);
-                printf("|");
-                gotoxy(80,l);
-                printf("|");
-            }
-            
-            for ( c = 1; c < 81; c++) {
-                gotoxy(c,01);
-                printf("-");
-                gotoxy(c,04);
-                printf("-");
-                gotoxy(c,22);
-                printf("-");
-                gotoxy(c,24);
-                printf("-");
-            }
-
-            gotoxy(01,1);
-            printf("+");
-            gotoxy(80,1);
-            printf("+");
-            gotoxy(1,22);
-            printf("+");
-            gotoxy(1,24);
-            printf("+");
-            gotoxy(80,24);
-            printf("+");
-            gotoxy(80,22);
-            printf("+");
-            gotoxy(01,4);
-            printf("+");
-            gotoxy(80,4);
-            printf("+");
-            gotoxy(2,2);
-            printf("UNICV");
-            gotoxy(2,3);
-            printf("Arthur Almeida Pereira 2 Semestre ADS/ESW");
-            gotoxy(62,2);
-            printf("Estrutura de Dados");
-            gotoxy(67,3);
-            printf("Prof. Rodney");
-            
-            gotoxy(24,5);
-            printf("TELA DE CADASTRO");
-            gotoxy(24,9);
-            printf("2 - Consultar o aluno");
-            gotoxy(24,11);
-            printf("3 - Editar o cadastro");
-            gotoxy(24,13);
-            printf("4 - Excluir cadastro");
-            gotoxy(24,17);
-            printf("5 - MENU");
-            gotoxy(02,23);
-            printf("MSG: ");
-            gotoxy(07,23);
-            scanf("%d", &opcao);
-
             system("cls");
 
             break;
@@ -241,48 +145,7 @@ int main (){
             case 2:
             system("cls");
             
-            for ( l = 1; l < 25; l++) {
-                gotoxy(01,l);
-                printf("|");
-                gotoxy(80,l);
-                printf("|");
-            }
-            
-            for ( c = 1; c < 81; c++) {
-                gotoxy(c,01);
-                printf("-");
-                gotoxy(c,04);
-                printf("-");
-                gotoxy(c,22);
-                printf("-");
-                gotoxy(c,24);
-                printf("-");
-            }
-
-            gotoxy(01,1);
-            printf("+");
-            gotoxy(80,1);
-            printf("+");
-            gotoxy(1,22);
-            printf("+");
-            gotoxy(1,24);
-            printf("+");
-            gotoxy(80,24);
-            printf("+");
-            gotoxy(80,22);
-            printf("+");
-            gotoxy(01,4);
-            printf("+");
-            gotoxy(80,4);
-            printf("+");
-            gotoxy(2,2);
-            printf("UNICV");
-            gotoxy(2,3);
-            printf("Arthur Almeida Pereira 2 Semestre ADS/ESW");
-            gotoxy(62,2);
-            printf("Estrutura de Dados");
-            gotoxy(67,3);
-            printf("Prof. Rodney");
+            tela();
             gotoxy(24,9);
             printf("Nome: %s", nome);
             gotoxy(24,11);
@@ -304,74 +167,31 @@ int main (){
             
             case 3:
             system("cls");
-            for ( l = 1; l < 25; l++) {
-                gotoxy(01,l);
-                printf("|");
-                gotoxy(80,l);
-                printf("|");
-            }
-            
-            for ( c = 1; c < 81; c++) {
-                gotoxy(c,01);
-                printf("-");
-                gotoxy(c,04);
-                printf("-");
-                gotoxy(c,22);
-                printf("-");
-                gotoxy(c,24);
-                printf("-");
-            }
-
-            gotoxy(01,1);
-            printf("+");
-            gotoxy(80,1);
-            printf("+");
-            gotoxy(1,22);
-            printf("+");
-            gotoxy(1,24);
-            printf("+");
-            gotoxy(80,24);
-            printf("+");
-            gotoxy(80,22);
-            printf("+");
-            gotoxy(01,4);
-            printf("+");
-            gotoxy(80,4);
-            printf("+");
-            gotoxy(2,2);
-            printf("UNICV");
-            gotoxy(2,3);
-            printf("Arthur Almeida Pereira 2 Semestre ADS/ESW");
-            gotoxy(62,2);
-            printf("Estrutura de Dados");
-            gotoxy(67,3);
-            printf("Prof. Rodney");
+            tela();
             
             gotoxy(24,5);
             printf("TELA DE CADASTRO");
 
             gotoxy(24,9);
             printf("Nome: ");
+            gotoxy(24,11);
+            printf("Idade: ");
+            gotoxy(24,13);
+            printf("Email: ");
 
             gotoxy(30,9);
             fflush(stdin);
             fgets(nome, 50, stdin);
 
-            gotoxy(24,11);
-            printf("Idade: ");
-
             gotoxy(31,11);
             scanf("%d", &idade);
-
-            gotoxy(24,13);
-            printf("Email: ");
 
             gotoxy(31,13);
             fflush(stdin);
             fgets(email, 50, stdin);
 
             gotoxy(24,15);
-            printf("Cadastro feito com sucesso!");
+            printf("Cadastro novo feito com sucesso!");
 
             gotoxy(24,17);
             printf("5 - MENU");
@@ -395,48 +215,7 @@ int main (){
             gotoxy(24,11);
             printf("Cadastro excluido com sucesso!\n");
 
-            for ( l = 1; l < 25; l++) {
-                gotoxy(01,l);
-                printf("|");
-                gotoxy(80,l);
-                printf("|");
-            }
-            
-            for ( c = 1; c < 81; c++) {
-                gotoxy(c,01);
-                printf("-");
-                gotoxy(c,04);
-                printf("-");
-                gotoxy(c,22);
-                printf("-");
-                gotoxy(c,24);
-                printf("-");
-            }
-
-            gotoxy(01,1);
-            printf("+");
-            gotoxy(80,1);
-            printf("+");
-            gotoxy(1,22);
-            printf("+");
-            gotoxy(1,24);
-            printf("+");
-            gotoxy(80,24);
-            printf("+");
-            gotoxy(80,22);
-            printf("+");
-            gotoxy(01,4);
-            printf("+");
-            gotoxy(80,4);
-            printf("+");
-            gotoxy(2,2);
-            printf("UNICV");
-            gotoxy(2,3);
-            printf("Arthur Almeida Pereira 2 Semestre ADS/ESW");
-            gotoxy(62,2);
-            printf("Estrutura de Dados");
-            gotoxy(67,3);
-            printf("Prof. Rodney");
+            tela();
             
             gotoxy(24,5);
             printf("TELA DE CADASTRO");
@@ -464,48 +243,7 @@ int main (){
 
             system("cls");
 
-                for ( l = 1; l < 25; l++) {
-                gotoxy(01,l);
-                printf("|");
-                gotoxy(80,l);
-                printf("|");
-            }
-            
-            for ( c = 1; c < 81; c++) {
-                gotoxy(c,01);
-                printf("-");
-                gotoxy(c,04);
-                printf("-");
-                gotoxy(c,22);
-                printf("-");
-                gotoxy(c,24);
-                printf("-");
-            }
-
-            gotoxy(01,1);
-            printf("+");
-            gotoxy(80,1);
-            printf("+");
-            gotoxy(1,22);
-            printf("+");
-            gotoxy(1,24);
-            printf("+");
-            gotoxy(80,24);
-            printf("+");
-            gotoxy(80,22);
-            printf("+");
-            gotoxy(01,4);
-            printf("+");
-            gotoxy(80,4);
-            printf("+");
-            gotoxy(2,2);
-            printf("UNICV");
-            gotoxy(2,3);
-            printf("Arthur Almeida Pereira 2 Semestre ADS/ESW");
-            gotoxy(62,2);
-            printf("Estrutura de Dados");
-            gotoxy(67,3);
-            printf("Prof. Rodney");
+            tela();
             
             gotoxy(24,5);
             printf("TELA DE CADASTRO");
@@ -517,8 +255,6 @@ int main (){
             printf("3 - Editar o cadastro");
             gotoxy(24,15);
             printf("4 - Excluir cadastro");
-            gotoxy(24,17);
-            printf("5 - MENU");
             gotoxy(24,19);
             printf("0 - Sair");
             gotoxy(02,23);
